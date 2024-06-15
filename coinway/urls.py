e40f87c2df366a1ae22011434e1cfe705fd2b296
JinkoSiz <zpska1977@mail.ru>
2024-06-15 12:59:29 +0300

    Adding Check-In

--- coinway/urls.py
+++ coinway/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
     path('users/', include('users.urls')),
+    path('checkin/', include('checkin.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

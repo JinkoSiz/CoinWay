44369334624a9894b50e7df743bff68b7ca5bd08
JinkoSiz <zpska1977@mail.ru>
2024-06-22 18:44:52 +0300

    Added News plus some fixes

--- coinway/urls.py
+++ coinway/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('projects.urls')),
     path('users/', include('users.urls')),
     path('checkin/', include('checkin.urls')),
+    path('game/', include('game.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

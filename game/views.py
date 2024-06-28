c8169120b3b6c45721fdc313f40f1a6096d28143
JinkoSiz <zpska1977@mail.ru>
2024-06-28 18:59:34 +0300

    Added EXP

--- game/views.py
+++ game/views.py
@@ -1,7 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from datetime import timedelta
+from django.contrib import messages
+import logging
+from users.models import Profile
+
+logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+@login_required
+def claim_exp(request):
+    logger.info("claim_exp view called")
+    profile = Profile.objects.get(user=request.user)
+    now = timezone.now()
+    time_remaining = None
+    next_claim_time_str = None
+
+    if request.method == 'POST':
+        logger.info("POST request received")
+
+        if profile.last_claimed is None or now - profile.last_claimed >= timedelta(hours=12):
+            profile.exp += 50  # Добавляем 50 опыта, можно изменить это значение
+            profile.last_claimed = now
+            profile.save()
+            message = "Вы успешно получили 50 опыта!"
+            logger.info("Experience claimed successfully")
+        else:
+            next_claim_time = profile.last_claimed + timedelta(hours=12)
+            time_remaining = next_claim_time - now
+            next_claim_time_str = str(time_remaining).split('.')[0]  # Убираем микросекунды
+            message = f"Get after {next_claim_time_str}"
+            logger.info(f"Cannot claim experience yet, time remaining: {next_claim_time_str}")
+
+        return render(request, 'game/game.html',
+                      {'message': message, 'exp': profile.exp, 'next_claim_time_str': next_claim_time_str, 'html_name': 'Игра'})
+
+    if profile.last_claimed:
+        next_claim_time = profile.last_claimed + timedelta(hours=12)
+        time_remaining = next_claim_time - now
+        next_claim_time_str = str(time_remaining).split('.')[0]  # Убираем микросекунды
+
+    return render(request, 'game/game.html', {'exp': profile.exp, 'next_claim_time_str': next_claim_time_str, 'html_name': 'Игра'})
+
+
 def game(request):
-    context = {'html_name': 'Игра'}
+    context = {'html_name': 'Игра', 'exp': request.user.profile.exp}
     return render(request, 'game/game.html', context)
